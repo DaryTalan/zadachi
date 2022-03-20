fed4818a8a8c9f631bbbf5c97844aa8f87f82c0e
DaryTalan <101998523+DaryTalan@users.noreply.github.com>
2022-03-21 02:14:30 +0300

    1 block
     первой задаче вы перепутали и в случае, если х больше у, вы говорите, что у - больший. Во второй задаче ваши условные операторы будут работать не корректно. Дело в том, что после проверки, не является ли х максимальным, проверку для у вы делаете уже с учетом того, что предыдущая не сработала, это верно. Но откуда вам известно, какое именно из двух условий в седьмой строке у вас не сработало? Откуда убежденность, что именно первое, где сказано, что х > у? В третьей задаче командное слово else необходимо разместить на том же уровне, где и if(), который его "породил" :) И я хотел бы напомнить, что ДЗ сдается в формате ссылки на ваш удаленный репозиторий, это важное условие!

---
+++ 1.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Chislo vvedite 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Chislo vvedite 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y)
+{
+Console.WriteLine("{0} Maximal: ", y);
+}
+else
+{
+Console.WriteLine("{0} Maximal: ", x);
+}
---
+++ 3.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Chislo dlya proverki kratnosti: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+
+    Console.WriteLine("chislo chetnoe"); 
+    else 
+    Console.WriteLine("chislo nechetnoe");
