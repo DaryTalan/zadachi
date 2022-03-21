3841406138a885a2e3ce8de67f314f9b61c6ef7b
DaryTalan <d-talanova@mail.ru>
2022-03-21 17:42:25 +0300

    git add

---
+++ 2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Chislo vvedite 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Chislo vvedite 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Chislo vvedite 3: ");
+
