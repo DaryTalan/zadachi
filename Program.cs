163529b561207d664e065e9515d473c00e2d4e8e
DaryTalan <101998523+DaryTalan@users.noreply.github.com>
2022-03-21 03:27:07 +0300

    13 zad

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Chislo: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Дано число: {x}");
+if (x / 100 > 0) System.Console.WriteLine($"Третьей является цифра {x % 10}");
+else System.Console.WriteLine($"У числа {x} нет третьей цифры");
+System.Console.WriteLine();
