241bf22a9709312d15d9aae9b4b1cb4f26b2fa3e
DaryTalan <d-talanova@mail.ru>
2022-03-22 15:18:04 +0300

    git add

--- 3urok-21/Program.cs
+++ 3urok-21/Program.cs
@@ -13,5 +13,5 @@ double z2 = Convert.ToDouble(Console.ReadLine());
 
     double d = Math.Pow((Math.Pow(x2 - x1, 2) +  Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) *  1.0), 0.5);
 
-Console.WriteLine("Distance is \n" + d);
+Console.WriteLine("Расстояние: \n" + d);
 return;
