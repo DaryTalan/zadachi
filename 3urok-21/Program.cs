8cd47766460258dacdea090182ba18727c25d554
DaryTalan <d-talanova@mail.ru>
2022-03-22 15:13:25 +0300

    git add

---
+++ 3urok-21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console. Write ("Введите через пробел координаты первой точки x: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console. Write ("Введите через пробел координаты первой точки y: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console. Write ("Введите через пробел координаты первой точки z: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console. Write ("Введите через пробел координаты второй точки x: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console. Write ("Введите через пробел координаты второй точки y: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console. Write ("Введите через пробел координаты второй точки z: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+    double d = Math.Pow((Math.Pow(x2 - x1, 2) +  Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2) *  1.0), 0.5);
+
+Console.WriteLine("Distance is \n" + d);
+return;
--- zadacha 13/Program.cs
+++ zadacha 13/Program.cs
@@ -1,6 +1,9 @@
 ﻿Console.Write("Chislo: ");
 int x = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine($"Дано число: {x}");
-if (x / 100 > 0) System.Console.WriteLine($"Третьей является цифра {x % 10}");
+if (x > 99999 && x < 999999) System.Console.WriteLine($"Третьей является цифра {x /1000 % 10}");
+else if (x > 9999 && x < 99999) System.Console.WriteLine($"Третьей является цифра {x /100 % 10}");
+else if (x > 999 && x < 9999) System.Console.WriteLine($"Третьей является цифра {x /10 % 10}");
+else if (x > 99) System.Console.WriteLine($"Третьей является цифра {x % 10}");
 else System.Console.WriteLine($"У числа {x} нет третьей цифры");
 System.Console.WriteLine();
