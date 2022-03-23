4752f9079fd8e6be155e06a112578f1bb2eaccc9
DaryTalan <d-talanova@mail.ru>
2022-03-24 01:32:50 +0300

    git add

---
+++ 3urok23/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите N для конечного значения: ");
+int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+            Console.Write(" {1}", i, Math.Pow(i,3));
+        System.Console.WriteLine();
